52f962e8e8550facc689e10dc3b79d4de14d2f33
hotnAny <xiangchen@acm.org>
2024-04-24 21:10:07 -0700

    logged mouse move

--- scripts/scott.js
+++ scripts/scott.js
@@ -22,26 +22,76 @@ class Buffer {
   all() {
     return this.buffer
   }
+
+  last() {
+    return this.buffer[this.buffer.length - 1]
+  }
 }
 
 const BUFFER_SIZE = 128
+const INTERVAL_MOUSE = 1000
 
 let _bufferMouseMove = new Buffer(BUFFER_SIZE)
+let _distMove = 0
+let _xPrev, _yPrev
+
+const onMouseDown = (e) => {
+  // _xPrev = e.clientX
+  // _yPrev = e.clientY
+
+  // _entryMouseMove = {
+  //   start: e.timeStamp,
+  //   end: undefined,
+  //   distMove: 0
+  // }
+}
 
 const onMouseMove = (e) => {
-    const entry = {
-        timeStamp: e.timeStamp,
-        x: e.clientX,
-        y: e.clientY
-    }
-    _bufferMouseMove.add(entry)
+  // const entry = {
+  //   timeStamp: e.timeStamp,
+  //   x: e.clientX,
+  //   y: e.clientY,
+  //   delta: Math.sqrt(Math.pow(e.clientX - _xPrev, 2) + Math.pow(e.clientY - _yPrev, 2))
+  // }
+
+  if (_xPrev != undefined && _yPrev != undefined) {
+    let delta = Math.sqrt(Math.pow(e.clientX - _xPrev, 2) + Math.pow(e.clientY - _yPrev, 2))
+
+    _distMove += delta
+  }
+  // if(entry.delta > 0) {
+  //   _bufferMouseMove.add(entry)
+  // }
+
+  _xPrev = e.clientX
+  _yPrev = e.clientY
 }
 
 const onMouseUp = (e) => {
-    console.log(_bufferMouseMove.all())
+  _entryMouseMove.end = e.timeStamp
+  console.log(_entryMouseMove)
+  _bufferMouseMove.add(_entryMouseMove)
+}
+
+const logMouseMoveActivities = () => {
+  let entry = {
+    timeStamp: Date.now(),
+    distMove: _distMove
+  }
+  console.log(entry)
+  _bufferMouseMove.add(entry)
+  _distMove = 0
+
+  setTimeout(() => {
+    logMouseMoveActivities()
+  }, INTERVAL_MOUSE);
 }
 
 (function () {
+  document.addEventListener("mousedown", onMouseDown)
   document.addEventListener("mousemove", onMouseMove)
   document.addEventListener("mouseup", onMouseUp)
+
+  logMouseMoveActivities()
+
 })();
